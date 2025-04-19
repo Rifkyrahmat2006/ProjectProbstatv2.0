8c81c3e247ce09d06a2b0ff4d98ffa37a3f816dc
Rifkyrahmat2006 <rifkyrahmat2006@gmail.com>
2025-04-20 01:53:24 +0700

    commit for ProjectProbstatV2

--- app.py
+++ app.py
@@ -664,4 +664,5 @@ def edit_subject(subject_key):
     return render_template('edit_subject.html', subject={'code': subject_key, 'name': SUBJECTS[subject_key]})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
